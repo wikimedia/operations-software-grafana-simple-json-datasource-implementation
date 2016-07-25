6a45c6122a0d235ba96397a219894e8f20fff881
addshore <addshorewiki@gmail.com>
2016-07-25 16:12:59 +0100

    Annotations from dashiki pages!

---
+++ README.md
@@ -0,0 +1,3 @@
+### WMF Grafana JSON Datasource
+
+This repo should be checked out as the public_html directory of the grafana-json-datasource tool.
---
+++ annotations/index.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * TODO
+ *  - Only provide annotations for the given range?
+ */
+
+if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) {
+	die( 'Request must be posted.' );
+}
+
+$requestBody = file_get_contents('php://input');
+$request = json_decode( $requestBody, true );
+
+if ( $request === null ) {
+	die( 'Bad request JSON.' );
+}
+
+$pageTitle = 'Dashiki:' . trim( strtok( $request['annotation']['query'], ' ' ), '#' );
+$pageUrl = 'https://meta.wikimedia.org/wiki/' . $pageTitle . '?action=raw';
+$pageBody = file_get_contents( $pageUrl );
+
+if( $pageBody === false ) {
+	die( 'Request to ' . $pageUrl . ' failed.' );
+}
+
+$data = json_decode( $pageBody, true );
+
+if ( $data === null ) {
+	die( 'Bad page JSON.' );
+}
+
+$response = array();
+
+foreach( $data as $annotationData ) {
+
+	if ( $annotationData['start'] == $annotationData['end'] ) {
+		$response[] = array(
+			'annotation' => $request['annotation'],
+			'title' => 'Event',
+			'time' => strval( strtotime( $annotationData['start'] ) ),
+			'text' => $annotationData['note'],
+		);
+	} else {
+		$response[] = array(
+			'annotation' => $request['annotation'],
+			'title' => 'Start Event',
+			'time' => strval( strtotime( $annotationData['start'] ) ),
+			'text' => 'Start: ' . $annotationData['note'],
+		);
+		$response[] = array(
+			'annotation' => $request['annotation'],
+			'title' => 'End Event',
+			'time' => strval( strtotime( $annotationData['end'] ) ),
+			'text' => 'End: ' . $annotationData['note'],
+		);
+	}
+}
+
+echo json_encode( $response );
