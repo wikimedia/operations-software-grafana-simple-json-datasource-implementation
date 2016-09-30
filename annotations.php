094ce25222ae95b337be36837989af361da3df10
addshore <addshorewiki@gmail.com>
2016-09-30 18:36:35 +0100

    annotations, also respond to OPTIONS request

--- annotations.php
+++ annotations.php
@@ -5,56 +5,64 @@
  *  - Only provide annotations for the given range?
  */
 
-if ( $_SERVER['REQUEST_METHOD'] !== 'POST' ) {
-	die( 'Request must be posted.' );
+if ( $_SERVER['REQUEST_METHOD'] === 'OPTIONS' ) {
+	header('Access-Control-Allow-Headers:accept, content-type');
+	header('Access-Control-Allow-Methods:POST');
+	header('Access-Control-Allow-Origin:*');
+	return;
 }
 
-$requestBody = file_get_contents('php://input');
-$request = json_decode( $requestBody, true );
 
-if ( $request === null ) {
-	die( 'Bad request JSON.' );
-}
+if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+	$requestBody = file_get_contents('php://input');
+	$request = json_decode( $requestBody, true );
 
-$pageTitle = 'Dashiki:' . trim( strtok( $request['annotation']['query'], ' ' ), '#' );
-$pageUrl = 'https://meta.wikimedia.org/wiki/' . $pageTitle . '?action=raw';
-$pageBody = file_get_contents( $pageUrl );
-
-if( $pageBody === false ) {
-	die( 'Request to ' . $pageUrl . ' failed.' );
-}
-
-$data = json_decode( $pageBody, true );
-
-if ( $data === null ) {
-	die( 'Bad page JSON.' );
-}
-
-$response = array();
-
-foreach( $data as $annotationData ) {
-
-	if ( $annotationData['start'] == $annotationData['end'] ) {
-		$response[] = array(
-			'annotation' => $request['annotation'],
-			'title' => 'Event',
-			'time' => strval( strtotime( $annotationData['start'] ) ),
-			'text' => $annotationData['note'],
-		);
-	} else {
-		$response[] = array(
-			'annotation' => $request['annotation'],
-			'title' => 'Start Event',
-			'time' => strval( strtotime( $annotationData['start'] ) ),
-			'text' => 'Start: ' . $annotationData['note'],
-		);
-		$response[] = array(
-			'annotation' => $request['annotation'],
-			'title' => 'End Event',
-			'time' => strval( strtotime( $annotationData['end'] ) ),
-			'text' => 'End: ' . $annotationData['note'],
-		);
+	if ( $request === null ) {
+		die( 'Bad request JSON.' );
 	}
+
+	$pageTitle = 'Dashiki:' . trim( strtok( $request['annotation']['query'], ' ' ), '#' );
+	$pageUrl = 'https://meta.wikimedia.org/wiki/' . $pageTitle . '?action=raw';
+	$pageBody = file_get_contents( $pageUrl );
+
+	if( $pageBody === false ) {
+		die( 'Request to ' . $pageUrl . ' failed.' );
+	}
+
+	$data = json_decode( $pageBody, true );
+
+	if ( $data === null ) {
+		die( 'Bad page JSON.' );
+	}
+
+	$response = array();
+
+	foreach( $data as $annotationData ) {
+
+		if ( $annotationData['start'] == $annotationData['end'] ) {
+			$response[] = array(
+				'annotation' => $request['annotation'],
+				'title' => 'Event',
+				'time' => strval( strtotime( $annotationData['start'] ) ),
+				'text' => $annotationData['note'],
+			);
+		} else {
+			$response[] = array(
+				'annotation' => $request['annotation'],
+				'title' => 'Start Event',
+				'time' => strval( strtotime( $annotationData['start'] ) ),
+				'text' => 'Start: ' . $annotationData['note'],
+			);
+			$response[] = array(
+				'annotation' => $request['annotation'],
+				'title' => 'End Event',
+				'time' => strval( strtotime( $annotationData['end'] ) ),
+				'text' => 'End: ' . $annotationData['note'],
+			);
+		}
+	}
+
+	echo json_encode( $response );
 }
 
-echo json_encode( $response );
+die( 'Request must be POST or OPTIONS.' );
